1ffa43413a77b2e247a014877706787bd69a472a
suyogmgr <mgrsuyog6@gmail.com>
2025-06-03 10:59:21 +0545

    onclickUpdate

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function show(element){
+/*function show(element){
     element.querySelector('.dropdown').classList.add('show');
 }
 
 function hide(element){
     element.querySelector('.dropdown').classList.remove('show');
-}
+}*/
 
 function slideOut(){
     document.getElementById('sidebar').classList.remove('show');
@@ -14,4 +14,10 @@ function slideOut(){
 function slideIn(){
     document.getElementById('sidebar').classList.remove('close');
     document.getElementById('sidebar').classList.add('show');
+}
+
+
+
+function list(element){
+    element.querySelector('.dropdown').classList.toggle('show');
 }
